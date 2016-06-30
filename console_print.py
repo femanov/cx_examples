4041b0ff8bbd5f86397557b03c5ad21f5f1f1210
femanov <femanov@gmail.com>
2016-06-30 09:59:07 +0600

    minor changes and refactoring

--- console_print.py
+++ console_print.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import pycx4.pycda as cda
-import pycx4.scheduler as sl
+
 
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
@@ -9,7 +9,7 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 def printval(chan):
     print(chan.val)
 
-chan = cda.schan("localhost:1.name.0")
+chan = cda.schan("ichw1-2:0.pwa1.Etoday")
 chan.valueMeasured.connect(printval)
 
-sl.main_loop()
+cda.main_loop()
