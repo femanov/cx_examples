5b903ce5b6106629c9faf112f24882df873db625
femanov <femanov@gmail.com>
2017-04-26 23:52:29 +0700

    chan initialized added.

--- console_print.py
+++ console_print.py
@@ -9,7 +9,7 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 def printval(chan):
     print(chan.val)
 
-chan = cda.Chan("ichw1-2:0.pwa1.Etoday")
+chan = cda.Chan("linac1:11.rst1.GUN_V.Imes")
 chan.valueMeasured.connect(printval)
 
 cda.main_loop()
