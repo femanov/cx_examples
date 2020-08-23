06164b7ba7aaf4091a054149984c47715742d83e
femanov <femanov@gmail.com>
2020-08-23 18:34:23 +0700

    1) implemented connection of signals to signals 2) implemented some some event processing code 3) compile-time directives sor signals removed 4) some old code removed

--- console_qt_test.py
+++ console_qt_test.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python3
-
 import sys
 import signal
 from PyQt5 import QtCore
-
-from pycx4.qcda import cda
+import pycx4.qcda as cda
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
-
 
 def printval(chan):
     print(chan.val)
 
-
 app = QtCore.QCoreApplication(sys.argv)
 
-
-c = cda.DChan('canhw:11.rings.imes')
+c = cda.DChan('canhw:13.S13_out.Imes')
 c.valueMeasured.connect(printval)
 
 app.exec_()
