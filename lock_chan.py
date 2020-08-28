8e9a7de11cac6f2e161026ea702b6e48a9b86bca
femanov <femanov@gmail.com>
2020-08-28 22:36:54 +0700

    loking test working

--- lock_chan.py
+++ lock_chan.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
 
+# in order to see locking work run 2 copies of this program
+
+
 import pycx4.pycda as cda
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+
 def printval(chan):
-    chan.lock()
+    chan.lock()  # here works good
     print(chan.val)
-    #chan
+    if chan.lock_state:
+        print("setting val")
+        chan.setValue(chan.val - 0.1)
+
 
 def locked(chan):
-    print('chan locked?', chan.lock_state)
+    if chan.lock_state:
+        print("chan locked, now I can work")
+        # initiate some work
+        chan.setValue(chan.val - 0.1)
+
 
 chan = cda.Chan("canhw:11.rings.Iset")
 chan.valueMeasured.connect(printval)
 chan.lockStateUpdate.connect(locked)
 
-
+#chan.lock() # <- not working here
 
 cda.main_loop()
