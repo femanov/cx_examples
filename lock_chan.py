7b87d61911e0bee91c4b406ea57770ef80e21711
femanov <femanov@gmail.com>
2020-09-02 16:56:27 +0700

    - some small fixes - temperature reader test from ds18b20 connected to raspberry pi,

--- lock_chan.py
+++ lock_chan.py
@@ -2,17 +2,15 @@
 
 # in order to see locking work run 2 copies of this program
 
-
 import pycx4.pycda as cda
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 
 def printval(chan):
-    chan.lock()  # here works good
-    print(chan.val)
     if chan.lock_state:
-        print("setting val")
+        print("cur_val=", chan.val)
+        print("s val")
         chan.setValue(chan.val - 0.1)
 
 
@@ -23,10 +21,10 @@ def locked(chan):
         chan.setValue(chan.val - 0.1)
 
 
-chan = cda.Chan("canhw:11.rings.Iset")
-chan.valueMeasured.connect(printval)
-chan.lockStateUpdate.connect(locked)
+chan1 = cda.Chan("canhw:11.rings.Iset")
+chan1.valueMeasured.connect(printval)
+chan1.lockStateUpdate.connect(locked)
 
-#chan.lock() # <- not working here
+chan1.lock()  # <- not working
 
 cda.main_loop()
