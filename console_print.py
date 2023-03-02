3f03e2376cf559c1fa10af1c1456a5ec8297785e
femanov <femanov@gmail.com>
2023-03-02 10:08:59 +0700

    examples corrected

--- console_print.py
+++ console_print.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
-
 import pycx4.pycda as cda
-
-
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 def printval(chan):
     print(chan.val)
 
-chan = cda.Chan("canhw:21.ring_current")
+chan = cda.Chan("ring_current")
 chan.valueMeasured.connect(printval)
 
 cda.main_loop()
--- sl_timer.py
+++ sl_timer.py
@@ -1,15 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import pycx4.pycda as cda
-
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-def print_t(timer):
+
+def print_t():
     print("time out")
 
 
-t = cda.Timer(1000000, 1)
-t.shot.connect(print_t)
+t = cda.Timer(1000)
+t.start()
+t.timeout.connect(print_t)
+
 
 cda.main_loop()
